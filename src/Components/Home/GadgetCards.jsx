a523c013d07b268764eeea9193b1707a9e5ac5cd
blAkh3Art <saklayen000@gmail.com>
2024-11-06 20:06:10 +0600

    Final commit to finish the project
    Some code fixing and replacing dummy text

--- src/Components/Home/GadgetCards.jsx
+++ src/Components/Home/GadgetCards.jsx
@@ -27,10 +27,7 @@ const GadgetCards = () => {
             ) : (
                 <div className="flex items-center justify-center flex-col space-y-6 p-10 bg-[#13131308] rounded-[12px]">
                     <img className="w-[9.5rem] mx-auto mb-4" src="https://i.postimg.cc/JzQV0Sf2/error.webp" alt="No data available" />
-                    <h2 className="font-bold text-3xl text-color-secondary mb-2">No Information Available</h2>
-                    <p className="text-xl text-color-secondary">
-                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a.
-                    </p>
+                    <h2 className="font-bold text-3xl text-color-secondary mb-2">No Product available right now</h2>
                 </div>
             )}
         </div>
